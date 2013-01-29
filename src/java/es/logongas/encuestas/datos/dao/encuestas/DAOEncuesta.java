c7b4a9ce510fd97f4c1f0bcba5d8dc8d1e0da660
Lorenzo <logongas@gmail.com>
2013-01-29 10:40:13 +0100

    Añadir los interfaces DAO

---
+++ src/java/es/logongas/encuestas/datos/dao/encuestas/DAOEncuesta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.logongas.encuestas.datos.dao.encuestas;
+
+import es.logongas.encuestas.modelo.encuestas.Encuesta;
+import es.logongas.ix3.persistencia.services.dao.GenericDAO;
+
+/**
+ *
+ * @author Lorenzo González
+ */
+public interface DAOEncuesta extends GenericDAO<Encuesta,Integer>  {
+    
+}
---
+++ src/java/es/logongas/encuestas/datos/dao/encuestas/DAOPregunta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.logongas.encuestas.datos.dao.encuestas;
+
+import es.logongas.encuestas.modelo.encuestas.Pregunta;
+import es.logongas.ix3.persistencia.services.dao.GenericDAO;
+
+/**
+ *
+ * @author Lorenzo González
+ */
+public interface DAOPregunta extends GenericDAO<Pregunta,Integer> {
+    
+}
