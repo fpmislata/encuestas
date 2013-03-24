bc781d0d6aaa904c93c2ca4584390e310a1ad1d0
Lorenzo <logongas@gmail.com>
2013-03-24 21:04:02 +0100

    Añadir la pantalla de estadísticas

---
+++ src/java/es/logongas/encuestas/modelo/estadisticas/Estadistica.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.logongas.encuestas.modelo.estadisticas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Lorenzo González
+ */
+public class Estadistica {
+    List<String> labels=new ArrayList<String>();
+    List<Double> data=new ArrayList<Double>();
+}
---
+++ src/java/es/logongas/encuestas/presentacion/controller/EstadisticasController.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.logongas.encuestas.presentacion.controller;
+
+import es.logongas.encuestas.modelo.estadisticas.Estadistica;
+import es.logongas.ix3.persistencia.services.dao.BussinessException;
+import es.logongas.ix3.persistencia.services.dao.BussinessMessage;
+import es.logongas.ix3.persistencia.services.dao.DAOFactory;
+import es.logongas.ix3.persistencia.services.dao.GenericDAO;
+import es.logongas.ix3.persistencia.services.metadata.MetaData;
+import es.logongas.ix3.persistencia.services.metadata.MetaDataFactory;
+import es.logongas.ix3.presentacion.controller.RESTController;
+import es.logongas.ix3.presentacion.json.JsonFactory;
+import es.logongas.ix3.presentacion.json.JsonWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Lorenzo González
+ */
+@Controller
+public class EstadisticasController {
+
+    @Autowired
+    DAOFactory daoFactory;
+    @Autowired
+    MetaDataFactory metaDataFactory;
+    @Autowired
+    ConversionService conversionService;
+    @Autowired
+    JsonFactory jsonFactory;
+
+    private static Log log = LogFactory.getLog(RESTController.class);
+
+/*
+    @RequestMapping(value = {"/estadisticas/{idEncuesta}/{idPregunta}"}, method = RequestMethod.GET)
+    public void estadisticasPregunta(HttpServletRequest httpRequest, HttpServletResponse httpServletResponse, @PathVariable("idEncuesta") int idEncuesta, @PathVariable("idPregunta") int idPregunta) {
+        try {
+            JsonWriter jsonWriter = jsonFactory.getJsonWriter(Estadistica.class);
+            Estadistica estadistica=new Estadistica();
+
+            String jsonOut = jsonWriter.toJson(estadistica);
+
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            httpServletResponse.setContentType("application/json; charset=UTF-8");
+            httpServletResponse.getWriter().println(jsonOut);
+        } catch (BussinessException ex) {
+            try {
+                String jsonOut = jsonFactory.getJsonWriter().toJson(ex.getBussinessMessages());
+
+                httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                httpServletResponse.setContentType("application/json; charset=UTF-8");
+                httpServletResponse.getWriter().println(jsonOut);
+            } catch (Exception ex2) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                httpServletResponse.setContentType("text/plain");
+                try {
+                    ex.printStackTrace(httpServletResponse.getWriter());
+                } catch (Exception ex3) {
+                    log.error("Falló al imprimir la traza", ex3);
+                }
+            }
+        } catch (Exception ex) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            httpServletResponse.setContentType("text/plain");
+            try {
+                ex.printStackTrace(httpServletResponse.getWriter());
+            } catch (Exception ex2) {
+                log.error("Falló al imprimir la traza", ex2);
+            }
+        }
+    }
+*/
+}
